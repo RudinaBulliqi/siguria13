d7c24129481f9be9713439b61af29402402cc080
Sara Vishi <vishisara@gmail.com>
2025-04-20 18:56:27 +0200

    Update AutoKeyDecrypt.java

--- Autokey/AutoKeyDecrypt.java
+++ Autokey/AutoKeyDecrypt.java
@@ -1,15 +1,27 @@
+public class AutoKeyDecrypt {
+private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 public static String decrypt(String cipherText, String primer) {
-    StringBuilder key = new StringBuilder(primer);
-    StringBuilder result = new StringBuilder();
+StringBuilder key = new StringBuilder(primer);
+StringBuilder result = new StringBuilder();
 
-    for (int i = 0; i < cipherText.length(); i++) {
-        int c = ALPHABET.indexOf(cipherText.charAt(i));
-        int k = ALPHABET.indexOf(key.charAt(i));
-        int p = (c - k + 26) % 26;
-        char decryptedChar = ALPHABET.charAt(p);
-        result.append(decryptedChar);
-        key.append(decryptedChar);
-    }
+for (int i = 0; i < cipherText.length(); i++) {
+int c = ALPHABET.indexOf(cipherText.charAt(i));
+int k = ALPHABET.indexOf(key.charAt(i));
+int p = (c - k + 26) % 26;
+char decryptedChar = ALPHABET.charAt(p);
+result.append(decryptedChar);
+key.append(decryptedChar);
+}
 
-    return result.toString();
+return result.toString();
+}
+
+public static void main(String[] args) {
+String encryptedText = "ENCRYPTEDTEXT";
+String key = "KEY";
+
+String decrypted = decrypt(encryptedText, key);
+System.out.println("Decrypted text: " + decrypted);
+}
 }
